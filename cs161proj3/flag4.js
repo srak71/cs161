bba2a3be52339e0a6d01dfa0e023fa51257ce781
Saransh Rakshak <srakshak@berkeley.edu>
2021-12-11 11:54:06 -0800

    shitty attempt of flag7

--- cs161proj3/flag4.js
+++ cs161proj3/flag4.js
@@ -1,2 +1,4 @@
-nicholas' OR 1=1 UNION SELECT token FROM sessions'nicholas
-nicholas' OR 1=1 UNION SELECT username FROM users WHERE username = 'nicholas
+// Search For File
+//nicholas'OR 1=1--'// remove 2nd ' in List File --> Search for a file
+// File To Upload
+<script></script>
