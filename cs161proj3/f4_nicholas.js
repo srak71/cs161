94b48773b75238d823d1aa8828c65c048c774c7b
Saransh Rakshak <srakshak@berkeley.edu>
2021-12-11 12:20:45 -0800

    shitty attempt of flag7

--- cs161proj3/f4_nicholas.js
+++ cs161proj3/f4_nicholas.js
@@ -1,5 +1,5 @@
 // Nicholas' token will not be in database, mut instead 'disguise' user dev as nicholas
 // Search For File
-//nicholas'OR 1=1--'// remove 2nd ' in List File --> Search for a file
+// nicholas'OR 1=1--'// remove 2nd ' in List File --> Search for a file
 // File To Upload
 <script>a</script>
